2b9c8e2793fec038aa0a2c8add1d02c6ef3d5729
runner <runner@Mac-1614763043669.local>
2021-03-03 09:24:38 +0000

    deploy version 7.0.0

--- Herow/Package.swift
+++ Herow/Package.swift
@@ -1,6 +1,3 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -16,13 +13,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Herow",
-            dependencies: []),
-        .testTarget(
-            name: "HerowTests",
-            dependencies: ["Herow"]),
+        .binaryTarget(name: "Herow",
+                      url: "https://github.com/herowio/test/blob/main/7.0.0/herow_sdk_ios.xcframework.zip?raw=true",
+                      checksum: "3b10a6ae27d89b9d78500d71c16568f92735a09f7a22e586bb4687074f743a9e"),
     ]
 )
