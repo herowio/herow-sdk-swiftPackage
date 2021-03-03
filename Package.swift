3053c9c2a7e8217f7d8c6c1362a49e58827ce41c
clebodam <clebodam@gmail.com>
2021-03-03 10:48:29 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,19 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "Herow",
+    name: "herow_sdk_ios",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Herow",
-            targets: ["Herow"]),
+            name: "herow_sdk_ios",
+            targets: ["herow_sdk_ios"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "Herow",
+        .binaryTarget(name: "herow_sdk_ios",
                       url: "https://github.com/herowio/test/blob/main/7.0.0/herow_sdk_ios.xcframework.zip?raw=true",
                       checksum: "82e43ae686cceeacf6977a3c1dcf54ee50991d90d8335235d352a9e95e5cc384"),
     ]
