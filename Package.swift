2a1dee10b91d57da7375f9a85b40d040d0421afe
runner <runner@Mac-1614854179285.local>
2021-03-04 10:44:04 +0000

    deploy version 7.0.0

--- Package.swift
+++ Package.swift
@@ -1,20 +1,21 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "Herow",
+    name: "herow_sdk_ios",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Herow",
-            targets: ["Herow"]),
+            name: "herow_sdk_ios",
+            targets: ["herow_sdk_ios"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "Herow",
+        .binaryTarget(name: "herow_sdk_ios",
                       url: "https://github.com/herowio/test/blob/main/7.0.0/herow_sdk_ios.xcframework.zip?raw=true",
-                      checksum: "6219b44bd2d2d695fa18d75a90672277ad5bb8663fa2c7ae8e6beaa65405cfb5"),
+                      checksum: "d181ea616af1f30910e12f2ede0f71563203ff2a55ec1f52f999ebe31ecf0f7a"),
     ]
 )
