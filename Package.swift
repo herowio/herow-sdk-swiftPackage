b3bf62b29fcd248022ce4e94a7a83a82d699c29f
runner <runner@Mac-1614935206992.local>
2021-03-05 09:14:48 +0000

    deploy version v7.0.0

--- Package.swift
+++ Package.swift
@@ -7,18 +7,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "herow_sdk_ios",
-            targets: ["herow_sdk_ios"]),
+            targets: ["herow_sdk_ios","Dependencies"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name "cocoaLumberJack",url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.7.0"),
     ],
     targets: [
+    .target(name: "Dependencies", dependencies: ["cocoaLumberJack"], path: "Dependencies")
         .binaryTarget(name: "herow_sdk_ios",
                       url: "https://github.com/herowio/herow-sdk-ios/releases/download/v7.0.0/herow_sdk_ios.xcframework.zip",
-                      checksum: "dd82407b913a96ef90d9f2007ce43b39d06ac702e6c47c7544ce980bc63a5f78"),
-                      dependencies: [
-                   .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.7.0"),
-                ],
+                      checksum: "478625f66c3ee91f43c04ea1d455f1bf5680806d67fc3814a77a5f236b9c8bb3"),
     ]
 )
