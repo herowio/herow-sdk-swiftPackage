6599fb6cd662a76d2e8eacee9d5cee9c326e96c8
clebodam <clebodam@gmail.com>
2021-03-05 09:11:43 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,11 +11,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.7.0"),
+       
     ],
     targets: [
         .binaryTarget(name: "herow_sdk_ios",
                       url: "https://github.com/herowio/herow-sdk-ios/releases/download/v7.0.0/herow_sdk_ios.xcframework.zip",
                       checksum: "e2d206a51b018e76f22b6e526d4b7bf56be2afc74070c637195f7aa6e10cf2a3"),
+        dependencies: [
+                   .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.7.0"),
+                ],
     ]
 )
