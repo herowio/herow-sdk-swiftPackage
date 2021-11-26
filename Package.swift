cf4b38f8c6eb972cfdb5721b78ec7efc4224c29b
runner <runner@Mac-1637949177526.local>
2021-11-26 18:16:31 +0000

    deploy version v7.1.0

--- Package.swift
+++ Package.swift
@@ -17,6 +17,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "herow_sdk_ios",
                       url: "https://github.com/herowio/herow-sdk-ios/releases/download/v7.1.0/herow_sdk_ios.xcframework.zip",
-                      checksum: "269490dd8e2df918c395a49e4686a7517e35b2c15a34d9fbfde36f0aabfc5520"),
+                      checksum: "bcaeb993228779b74cc5fa39bb321dc5549d77b61ac05a3df976a2eb95d0636c"),
     ]
 )
